ff66d84bd268e81eb1348480f9432faccdac236c
Alifdanish74 <alifdanish74@gmail.com>
2022-04-06 12:43:22 +0800

    finish add toggle button

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:intl/intl.dart';
 import 'package:flutter_contactlist/Contact.dart';
 import 'package:http/http.dart' as http;
@@ -23,36 +24,57 @@ class HomepageState extends State<Homepage> {
   // DateFormat dateTimeFormat = DateFormat("yyyy--MM-dd HH:mm:ss");
   DateFormat dateTimeFormat = DateFormat("dd-MM-yyyy HH:mm:ss");
   late Future<List<contact>> _contact;
-  int Length = 0;
+  final ScrollController scrollController = ScrollController();
+  int snapLength = 0;
+  int half = 0;
   int totalLength = 0;
+  List<String> timeAgoList = [];
+  bool loadingVisible = false;
+  bool endIndicatorVisible = false;
+  int counter = 0;
+  List<bool> isSelected = [true, false];
 
   @override
   void initState() {
     super.initState();
     _contact = _fetchContactData();
+    getViewState();
+    scrollController.addListener(() {
+      if (scrollController.position.pixels ==
+              scrollController.position.maxScrollExtent &&
+          scrollController.position.userScrollDirection ==
+              ScrollDirection.reverse) {
+        counter++;
+        loadMoreData();
+      }
+    });
   }
 
 //Function needed in build context
 
   saveViewState() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    setState(() {
-      prefs.setStringList(
-        "isSelected",
-        isSelected.map((e) => e ? 'true' : 'false').toList(),
-      );
-    });
+    setState(
+      () {
+        prefs.setStringList(
+          "isSelected",
+          isSelected.map((e) => e ? 'true' : 'false').toList(),
+        );
+      },
+    );
   }
 
   getViewState() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    setState(() {
-      isSelected = (prefs
-              .getStringList('isSelected')
-              ?.map((e) => e == 'true' ? true : false)
-              .toList() ??
-          [true, false]);
-    });
+    setState(
+      () {
+        isSelected = (prefs
+                .getStringList('isSelected')
+                ?.map((e) => e == 'true' ? true : false)
+                .toList() ??
+            [true, false]);
+      },
+    );
   }
 
   userLogo() {
@@ -66,12 +88,10 @@ class HomepageState extends State<Homepage> {
     );
   }
 
-  List<contact> _newContact = [];
-  List<String> timeAgoList = [];
-  List<bool> isSelected = [true, false];
-
   // Function to fetch contact data from URL
   Future<List<contact>> _fetchContactData() async {
+    List<contact> _newContact = [];
+
     var url = 'https://contactlistdata.000webhostapp.com/';
     final response = await http.get(Uri.parse(url));
 
@@ -81,11 +101,11 @@ class HomepageState extends State<Homepage> {
           .map<contact>((json) => contact.fromJson(json))
           .toList();
 
-      Length = _newContact.length;
-      int half = (_newContact.length / 2).round();
+      snapLength = _newContact.length;
+      half = (_newContact.length / 2).round();
       totalLength = _newContact.length < half ? _newContact.length : half;
 
-      for (int i = 0; i < Length; i++) {
+      for (int i = 0; i < snapLength; i++) {
         String dateTimedataString;
 
         dateTimedataString = _newContact[i].getCheckInDate()!;
@@ -111,9 +131,71 @@ class HomepageState extends State<Homepage> {
       setState(() {
         timeAgoList = timeAgoList;
       });
+
       return _newContact;
     }
     return _newContact;
+  }
+
+  loadMoreData() {
+    setState(() {
+      loadingVisible = true;
+    });
+
+    Future.delayed(Duration(seconds: 2), () {
+      if (totalLength != snapLength) {
+        totalLength = snapLength;
+        setState(() {
+          loadingVisible = false;
+        });
+      } else {
+        setState(() {
+          loadingVisible = false;
+          endIndicatorVisible = true;
+
+          Future.delayed(Duration(seconds: 2), () {
+            setState(() {
+              endIndicatorVisible = false;
+            });
+          });
+        });
+      }
+    });
+  }
+
+  toggleButtons() {
+    return ToggleButtons(
+      children: const <Widget>[
+        // first toggle button
+        Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Text(
+            'Original Time',
+          ),
+        ),
+        // second toggle button
+        Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Text(
+            'Time ago',
+          ),
+        )
+      ],
+      // logic for button selection below
+      onPressed: (int index) {
+        setState(() {
+          for (int i = 0; i < isSelected.length; i++) {
+            if (i == index) {
+              isSelected[i] = true;
+            } else {
+              isSelected[i] = false;
+            }
+          }
+          saveViewState();
+        });
+      },
+      isSelected: isSelected,
+    );
   }
 
   @override
@@ -125,39 +207,78 @@ class HomepageState extends State<Homepage> {
       body: Stack(
         children: [
           Positioned.fill(
-              top: 60,
-              bottom: 60,
+            child: Container(
+              margin: EdgeInsets.all(5),
+              height: 50,
               child: Align(
-                  alignment: Alignment.center,
-                  child: FutureBuilder(
-                      initialData: [],
-                      future: _contact,
-                      builder: (context, AsyncSnapshot snapshot) {
-                        return snapshot.hasData
-                            ? ListView.builder(itemBuilder: (context, index) {
-                                return Card(
-                                  child: ListTile(
-                                    leading: userLogo(),
-                                    title: Text(
-                                      snapshot.data[index].getName(),
-                                      style: TextStyle(fontSize: 20),
-                                    ),
-                                    subtitle: Column(children: [
-                                      textField(
-                                          snapshot.data[index].getPhone()),
-                                      Visibility(
-                                          visible: isSelected[0],
-                                          child: textField(snapshot.data[index]
-                                              .getCheckInDate())),
-                                      Visibility(
-                                          visible: isSelected[1],
-                                          child: textField(timeAgoList[index]))
-                                    ]),
-                                  ),
-                                );
-                              })
-                            : const Center(child: CircularProgressIndicator());
-                      })))
+                alignment: Alignment.topRight,
+                child: toggleButtons(),
+              ),
+            ),
+          ),
+          Positioned.fill(
+            top: 60,
+            bottom: 60,
+            child: Align(
+              alignment: Alignment.center,
+              child: FutureBuilder(
+                initialData: [],
+                future: _contact,
+                builder: (context, AsyncSnapshot snapshot) {
+                  return snapshot.hasData
+                      ? ListView.builder(
+                          physics: const BouncingScrollPhysics(
+                              parent: AlwaysScrollableScrollPhysics()),
+                          controller: scrollController,
+                          itemCount: totalLength,
+                          itemBuilder: (context, index) {
+                            return Card(
+                              child: ListTile(
+                                leading: userLogo(),
+                                title: Text(
+                                  snapshot.data[index].getName(),
+                                  style: TextStyle(fontSize: 20),
+                                ),
+                                subtitle: Column(children: [
+                                  textField(snapshot.data[index].getPhone()),
+                                  Visibility(
+                                      visible: isSelected[0],
+                                      child: textField(snapshot.data[index]
+                                          .getCheckInDate())),
+                                  Visibility(
+                                      visible: isSelected[1],
+                                      child: textField(timeAgoList[index]))
+                                ]),
+                              ),
+                            );
+                          })
+                      : const Center(child: CircularProgressIndicator());
+                },
+              ),
+            ),
+          ),
+          Positioned.fill(
+            bottom: 20,
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Visibility(
+                visible: endIndicatorVisible,
+                child: const Text('You have reached end of the list'),
+              ),
+            ),
+          ),
+          Positioned.fill(
+            bottom: 20,
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Visibility(
+                visible: loadingVisible,
+                child: const CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
